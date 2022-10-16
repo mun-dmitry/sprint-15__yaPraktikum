a38ca65d95ed15ea78717c50e1c2198a6432befe
Dmitry Mun <mundmitry@gmail.com>
2022-10-16 20:51:52 +0300

    Added and configured node modules helmet, cors and express-rate-limiter

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const { celebrate, Joi } = require('celebrate');
+const rateLimit = require('express-rate-limit');
 
 const router = require('./routes/router.js');
 const { login, createUser } = require('./controllers/users.js');
@@ -13,6 +16,10 @@ const { requestLogger, errorLogger } = require('./middlewares/logger.js');
 const urlValidationHelper = require('./middlewares/urlValidationHelper');
 
 const { PORT = 3000 } = process.env;
+const limiter = rateLimit({
+  windowsMs: 15 * 60 * 1000,
+  max: 100,
+});
 
 const app = express();
 
@@ -26,9 +33,12 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useUnifiedTopology: true,
 });
 
+app.use(helmet());
+app.use(limiter);
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(requestLogger);
+app.use(cors({ credentials: true }));
 
 app.get('/crash-test', () => {
   setTimeout(() => {
